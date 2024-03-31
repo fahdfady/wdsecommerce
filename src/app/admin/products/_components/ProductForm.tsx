8d5978c7d28208dbcfad9b633ad7bc07770219cd
fahdfady <fahd.fady212@gmail.com>
2024-04-01 00:35:08 +0200

    finish add product form

--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -7,15 +7,17 @@ import { Textarea } from "@/components/ui/textarea"
 import { formatCurrency } from "@/lib/formatters"
 import { useState } from "react"
 import { addProduct } from "../../_actions/products"
+import { useFormState, useFormStatus } from "react-dom"
 
 export function ProductForm() {
+    const [error, action] = useFormState(addProduct, {})
     const [priceInCents, setPriceInCents] = useState<number>()
-
     return (
-        <form action={addProduct} className="space-y-8">
+        <form action={action} className="space-y-8">
             <div className="space-y-2">
                 <Label htmlFor="name">Name</Label>
                 <Input type="text" id="name" name="name" required />
+                {error.name && <p className="text-destructive">{error.name}</p>}
             </div>
 
             <div className="space-y-2">
@@ -25,24 +27,36 @@ export function ProductForm() {
                     value={priceInCents} onChange={(e) => setPriceInCents(Number(e.target.value) || undefined)} />
             </div>
 
-            <div className="text-muted-foreground">{formatCurrency((priceInCents || 0) / 100)}</div>
+            <div className="text-muted-foreground">
+                {formatCurrency((priceInCents || 0) / 100)}
+                {error.priceInCents && <p className="text-destructive">{error.priceInCents}</p>}
+            </div>
 
             <div className="space-y-2">
                 <Label htmlFor="description">Description</Label>
                 <Textarea id="description" name="description" required />
+                {error.description && <p className="text-destructive">{error.description}</p>}
             </div>
 
             <div className="space-y-2">
                 <Label htmlFor="file">File</Label>
                 <Input type="file" id="file" name="file" required />
+                {error.file && <p className="text-destructive">{error.file}</p>}
             </div>
 
             <div className="space-y-2">
                 <Label htmlFor="image">Image</Label>
                 <Input type="file" id="image" name="image" required />
+                {error.image && <p className="text-destructive">{error.image}</p>}
             </div>
 
-            <Button type="submit">Save</Button>
+            <SubmitButton />
         </form>
     )
+} 
+
+function SubmitButton() {
+    const { pending } = useFormStatus()
+
+    return <Button type="submit" disabled={pending}> {pending ? "Saving..." : "Save"}</Button>
 }
